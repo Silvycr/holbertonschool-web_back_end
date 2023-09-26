48f7c9e9844ccac7d2baeaf65abac207f0c34738
Silvycr <silvia_alinacr@live.com>
2023-09-26 10:16:08 -0700

    3. Basic annotations - to string

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-""" script that returns a string """
+""" Float To STR """
 
 
 def to_str(n: float) -> str:
-    """ function that returns a string of a float value number"""
+    """ Float To STR """
     return str(n)
