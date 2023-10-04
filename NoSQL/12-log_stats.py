c698d0a1d53797fd79600391c08d234f773cf472
Silvycr <silvia_alinacr@live.com>
2023-10-03 20:08:54 -0700

    Log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
-""" provides some log data """
+"""logs stats"""
 from pymongo import MongoClient
 
-if __name__ == "__main__":
-    """ logs db and nginx collection hence client.logs.nginx """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
 
-    n_logs = nginx_collection.count_documents({})
-    print(f'{n_logs} logs')
+def loggedStats():
+    """logs the stats"""
+    client = MongoClient()
+    db = client.logs
+    coll = db.nginx
+    print("{} logs".format(coll.count_documents({})))
+    print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print('Methods:')
     for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f'\tmethod {method}: {count}')
+        print(f"\tmethod {method}: " +
+              f"{coll.count_documents({'method': method})}")
+    print(f"{coll.count_documents({'method': 'GET', 'path': '/status'})} \
+status check")
 
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f'{status_check} status check')
+
+if __name__ == "__main__":
+    loggedStats()
